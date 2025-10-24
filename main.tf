14d0d28974bf253ec6f86701b555d0614f7803dd
Akshay Pawar <akshaypawar@Akshays-MacBook-Air.local>
2025-10-24 18:36:52 +0530

    added userdata in module

--- main.tf
+++ main.tf
@@ -4,8 +4,10 @@ resource "aws_instance" "this" {
   key_name               = var.key_name
   vpc_security_group_ids = var.security_groups
   subnet_id              = var.subnet_id
+  user_data              = var.user_data 
 
   tags = {
     Name = var.instance_name
   }
 }
+
